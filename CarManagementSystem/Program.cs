27d1cefdb04c90e9dc8151183a543612884934a6
Pavel Svec <pavel.svec@itcrawl.cz>
2019-12-12 01:18:27 +0100

    Přidaný timer a omezení, diagnostika.

--- CarManagementSystem/Program.cs
+++ CarManagementSystem/Program.cs
@@ -1,7 +1,16 @@
 ﻿using System;
+using System.Timers;
+using System.Diagnostics;
 
 namespace CarManagementSystem
 {
+    public enum TypOmezeni
+    {
+        Zadne,
+        Tunel,
+        Most
+    }
+
     public enum StavAuta
     {
         Start,
@@ -34,11 +43,20 @@ namespace CarManagementSystem
     }
     public class Omezeni
     {
+        public Omezeni(TypOmezeni omezeni, double zacatek, double konec)
+        {
+            typOmezeni = omezeni;
+            this.zacatek = zacatek;
+            this.konec = konec;
+        }
+        TypOmezeni typOmezeni;
         double zacatek, konec;
     }
+
     public delegate void ZmenaStavuAuta(object sender, AutoInfo inf);
     public delegate void ZmenaPocasi(object sender, PocasiInfo inf);
     public delegate void ZmenRychlost(double delta);
+
     class RC
     {
         Auto[] registr;
@@ -51,6 +69,13 @@ namespace CarManagementSystem
         public void Add(Auto a)
         {
             registr[idxPosledniAuto++] = a;
+        }
+
+        public void AddTimerToFleet(Timer t)
+        {
+            foreach (Auto a in registr)
+                if (a != null)
+                    t.Elapsed += a.Check;
         }
 
         public void HromadnaInstrukce(double deltaV, bool svetla = false)
@@ -109,11 +134,18 @@ namespace CarManagementSystem
         public double trasa, aktualniRychlost;
         bool svetla;
         Omezeni[] omezeni;
+        private double CasNaCeste { get; set; } 
+        public double Ujeto { get; set; }
 
-        public Auto()
+        public Auto(double rychlost, double trasa)
         {
             omezeni = new Omezeni[100];
             id = Guid.NewGuid();
+            Ujeto = 0.0;
+            svetla = false;
+            trasa = 0.0;
+            aktualniRychlost = rychlost;
+            this.trasa = trasa;
         }
         public void PridejOmezeni(Omezeni o)
         {
@@ -128,6 +160,14 @@ namespace CarManagementSystem
         public void SnizRychlost(double deltaV)
         {
             this.aktualniRychlost -= deltaV;
+            Debug.WriteLine($"Změna rychlosti o {deltaV}");
+        }
+
+        public void Check(object sender, ElapsedEventArgs e)
+        {
+            CasNaCeste += 1.0;
+            Ujeto += aktualniRychlost * 1.0;
+            Debug.WriteLine($"Auto: {this.id}, Ujeto {this.Ujeto}, Rychlost {this.aktualniRychlost}");
         }
     }
 
@@ -135,26 +175,43 @@ namespace CarManagementSystem
     {
         public event ZmenaPocasi Zmena;
 
-        public void Check()
+        
+        public void Check(object sender, ElapsedEventArgs e)
         {
-            PocasiInfo pocI = new PocasiInfo() { pocasi = Pocasi.Sucho, teplota = 20.0 };
-            Zmena(this, pocI);
+            if (e.SignalTime.Ticks % 125 == 0)
+            {
+                PocasiInfo pocI = new PocasiInfo() { pocasi = Pocasi.Sucho, teplota = 20.0 };
+                Zmena(this, pocI);
+            }
+            else if (e.SignalTime.Ticks % 105 == 0)
+            {
+                PocasiInfo pocI = new PocasiInfo() { pocasi = Pocasi.Mraz, teplota = -10.0 };
+                Zmena(this, pocI);
+            }
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-
-            Meteo meteo = new Meteo();
-            RC ridici = new RC(100);
-            ridici.Add(new Auto());
-            ridici.Add(new Auto());
-            ridici.Add(new Auto());
-            ridici.Add(new Auto());
-            ridici.Add(new Auto());
-            ridici.SubscribeMeteo(meteo);
             
+            Meteo meteo = new Meteo();
+            
+            RC ridici = new RC(100);
+            Auto a1 = new Auto(3.5, 100.0);
+            a1.PridejOmezeni(new Omezeni(TypOmezeni.Most, 10.0, 11.5));
+            ridici.Add(a1);
+            ridici.Add(new Auto(2.5, 100.0));
+            ridici.Add(new Auto(3.0, 100.0));
+            ridici.Add(new Auto(2.1, 250.0));
+            ridici.Add(new Auto(3.7, 200.0));
+
+            ridici.SubscribeMeteo(meteo);
+            Timer ticker = new Timer(500);
+            ticker.Elapsed += meteo.Check;
+            ridici.AddTimerToFleet(ticker);
+            ticker.Start();
+            System.Threading.Thread.Sleep(20000);
         }
     }
 }
